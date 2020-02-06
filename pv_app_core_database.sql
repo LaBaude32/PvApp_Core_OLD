f0f480848fc13d4daf8ff665ea7de3904bd99c64
LaBaude32 <baudouin.coupey@gmail.com>
2020-02-06 11:05:43 +0100

    MAJ DBBD

--- pv_app_core_database.sql
+++ pv_app_core_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 04 fév. 2020 à 15:16
+-- Généré le :  jeu. 06 fév. 2020 à 10:04
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `affaire` (
 
 INSERT INTO `affaire` (`id_affaire`, `nom`, `adresse`, `avancement`, `type_reu`) VALUES
 (1, 'Layrac', 'Place du village', 15, 'Chantier'),
-(2, 'Castera-Lectourois', 'Rue principale', 100, 'Etude');
+(2, 'Castera ', 'Rue principale', 100, 'Etude');
 
 -- --------------------------------------------------------
 
@@ -75,7 +75,10 @@ CREATE TABLE IF NOT EXISTS `item` (
 DROP TABLE IF EXISTS `item_has_pv`;
 CREATE TABLE IF NOT EXISTS `item_has_pv` (
   `item_id` int(11) NOT NULL,
-  `pv_id` int(11) NOT NULL
+  `pv_id` int(11) NOT NULL,
+  PRIMARY KEY (`item_id`,`pv_id`),
+  KEY `fk_item_has_pv_pv1_idx` (`pv_id`),
+  KEY `fk_item_has_pv_item1_idx` (`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -91,17 +94,17 @@ CREATE TABLE IF NOT EXISTS `lot` (
   `affaire_id` int(11) NOT NULL,
   PRIMARY KEY (`id_lot`),
   KEY `fk_lot_affaire1_idx` (`affaire_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `lot`
 --
 
 INSERT INTO `lot` (`id_lot`, `name`, `affaire_id`) VALUES
-(2, 'Mobilier', 2),
-(3, 'Gros oeuvre', 2),
-(4, 'Plantation', 1),
-(5, 'Reseaux', 1);
+(1, 'Gros oeuvre', 1),
+(2, 'Mobilier', 1),
+(3, 'Plantation', 2),
+(4, 'Réseaux', 2);
 
 -- --------------------------------------------------------
 
@@ -128,8 +131,8 @@ CREATE TABLE IF NOT EXISTS `personne` (
 --
 
 INSERT INTO `personne` (`id_personne`, `email`, `password`, `first_name`, `last_name`, `telephone`, `groupe`, `fonction`, `organisme`) VALUES
-(1, 'baudcoupey@hotmail.fr', '1234', 'Baudouin', 'COUPEY', '0695847559', 'DevGroup', 'Deveuloppeur', 'BaudDev'),
-(2, 'contact@agencecasals.fr', 'azer', 'Samuel', 'COUPEY', '0562285081', 'Maitre d\'oeuvre', 'Paysagiste', 'Agence Casals');
+(1, 'baudcoupey@hotmail.fr', '1234', 'Baudouin', 'Coupey', '0695847559', 'Développement', 'Développeur', 'SAS Baud Dev'),
+(2, 'contact@agencecasals.fr', 'azer', 'Samuel', 'Coupey', '0562285081', 'Maitre d\'oeuvre', 'Architecte', 'Agence Casals');
 
 -- --------------------------------------------------------
 
@@ -171,14 +174,21 @@ CREATE TABLE IF NOT EXISTS `pv` (
 --
 
 INSERT INTO `pv` (`id_pv`, `etat`, `date_reunion`, `lieu_reunion`, `date_pro_reunion`, `lieu_pro_reunion`, `affaire_id`) VALUES
-(1, 'En cours', '2020-02-03 10:39:38', 'Sur place', '2020-02-03 10:39:38', 'En mairie', 1),
-(2, 'En cours', '2020-02-05 10:00:00', 'En mairie', '2020-02-10 10:00:00', 'Sur place', 1),
-(3, 'En cours', '2020-03-05 10:00:00', 'En mairie', '2020-03-10 10:00:00', 'Sur place', 2),
-(4, 'En cours', '2020-03-05 10:00:00', 'Sur place', '2020-03-10 10:00:00', 'Sur place', 2);
+(1, 'Terminé', '2020-02-04 09:30:00', 'En Mairie', '2020-02-06 09:30:00', 'Sur place', 1),
+(2, 'En cours', '2020-02-04 09:30:00', 'En Mairie', '2020-02-06 09:30:00', 'Sur place', 1),
+(3, 'En cours', '2020-02-02 15:00:00', 'Sur place', '2020-02-04 15:00:00', 'Sur place', 2),
+(4, 'En cours', '2020-02-04 15:00:00', 'Sur place', '2020-02-10 15:00:00', 'En mairie', 2);
 
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `item_has_pv`
+--
+ALTER TABLE `item_has_pv`
+  ADD CONSTRAINT `fk_item_has_pv_item1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id_item`),
+  ADD CONSTRAINT `fk_item_has_pv_pv1` FOREIGN KEY (`pv_id`) REFERENCES `pv` (`id_pv`);
 
 --
 -- Contraintes pour la table `lot`
