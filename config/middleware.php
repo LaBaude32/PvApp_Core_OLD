855c54225ebc45ccba55a8780edeb7ec1c10d752
Baudouin Coupey <baudouin.coupey@gmail.com>
2020-02-19 16:23:48 +0100

    Mise en place d'un middleware d'entrée pour le teste de connection

--- config/middleware.php
+++ config/middleware.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Middleware\LoginMiddleware;
 use Selective\Config\Configuration;
 use Slim\App;
 
@@ -11,12 +12,14 @@ return function (App $app) {
     $app->addRoutingMiddleware();
 
     $container = $app->getContainer();
-    
+
     // Add error handler middleware
     $settings = $container->get(Configuration::class)->getArray('error_handler_middleware');
-    $displayErrorDetails = (bool)$settings['display_error_details'];
-    $logErrors = (bool)$settings['log_errors'];
-    $logErrorDetails = (bool)$settings['log_error_details'];
+    $displayErrorDetails = (bool) $settings['display_error_details'];
+    $logErrors = (bool) $settings['log_errors'];
+    $logErrorDetails = (bool) $settings['log_error_details'];
 
     $app->addErrorMiddleware($displayErrorDetails, $logErrors, $logErrorDetails);
+
+    $app->add(new LoginMiddleware());
 };
---
+++ src/Middleware/LoginMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class LoginMiddleware
+{
+    /**
+     * Example middleware invokable class
+     *
+     * @param  ServerRequest  $request PSR-7 request
+     * @param  RequestHandler $handler PSR-15 request handler
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        $response = $handler->handle($request);
+        // $response->getBody()->write('BEFORE');
+        //TODO: faire un test de login ici, verifier la présence du tocken et la date de validité
+        return $response;
+    }
+}
