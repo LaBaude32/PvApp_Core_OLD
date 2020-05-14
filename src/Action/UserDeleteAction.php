b242d582c17a5cfa71ed0c100ac8bc08a0af05a5
Baudouin Coupey PopOs <baudouin.coupey@gmail.com>
2020-05-14 18:24:49 +0200

    Correction sur la suppression des user. -> amélioration du message de retour -> récupération des params et non du parsedbody

--- src/Action/UserDeleteAction.php
+++ src/Action/UserDeleteAction.php
@@ -18,14 +18,14 @@ final class UserDeleteAction
   public function __invoke(ServerRequest $request, Response $response): Response
   {
     // Collect input from the HTTP request
-    $data = (array) $request->getParsedBody();
+    $data = (array) $request->getQueryParams();
 
     $id = (int) $data['id_user'];
 
     // Invoke the Domain with inputs and retain the result
     $this->userDeletor->deleteUser($id);
 
-    $result = ["Le user a bien été supprimé"];
+    $result = "success";
 
     // Build the HTTP response
     return $response->withJson($result)->withStatus(201);
