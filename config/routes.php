45b80d5d5726fe2e3809bcd31042a8316465d8a2
Baudouin Coupey PopOs <baudouin.coupey@gmail.com>
2021-01-09 14:48:36 +0100

    fix OPTION route

--- config/routes.php
+++ config/routes.php
@@ -7,23 +7,27 @@ use App\Action\PreflightAction;
 
 return function (App $app) {
     // This route must not be protected
+    $app->get('/', \App\Action\HomeAction::class);
     $app->post('/api/v1/tokens', \App\Action\TokenCreateAction::class);
-    $app->options('/api/v1/tokens', \App\Action\PreflightAction::class);
     $app->post('/api/v1/addNewUser', \App\Action\UserCreateAction::class);
-    $app->options('/api/v1/addNewUser', \App\Action\PreflightAction::class);
+    $app->options('/api/v1/tokens', PreflightAction::class);
+    $app->options('/api/v1/addNewUser', PreflightAction::class);
+    $app->options('/api/v1/login', PreflightAction::class);
+    $app->options('/api/v1/getAllUsers', PreflightAction::class);
+    $app->options('/api/v1/addAffair', PreflightAction::class);
+    $app->options('/api/v1/addPv', PreflightAction::class);
+    $app->options('/api/v1/getLastPvsByUserId', PreflightAction::class);
 
-    //TODO: Traiter cette route correctement
-
+    //Protected routes
     $app->group('/api/v1', function (RouteCollectorProxy $group) {
         $group->post('/login', \App\Action\LoginAction::class);
-        $group->options('/login', PreflightAction::class);
+
         // $group->options('/login', App\Action\PreflightAction::class);
 
         $group->get('/', \App\Action\HomeAction::class);
         //User
         $group->post('/addUser', \App\Action\UserCreateAction::class);
         $group->get('/getAllUsers', \App\Action\UsersGetAllAction::class);
-        // $group->options('/getAllUsers', App\Action\PreflightAction::class);
         $group->post('/updateUser', \App\Action\UserUpdateAction::class);
         $group->post('/updateParticipant', \App\Action\ParticipantUpdateAction::class);
         $group->delete('/deleteUser', \App\Action\UserDeleteAction::class);
@@ -31,13 +35,10 @@ return function (App $app) {
 
         //Affair
         $group->post('/addAffair', \App\Action\AffairCreateAction::class);
-        $group->options('/addAffair', PreflightAction::class);
-
         $group->get('/getAllAffairs', \App\Action\AffairsGetAllAction::class);
         $group->get('/getAffairById', \App\Action\AffairGetByIdAction::class);
         //TODO: get Affair By User Id
         $group->get('/getAffairsByUserId', \App\Action\AffairsGetByUserIdAction::class);
-
         $group->post('/updateAffair', \App\Action\AffairUpdateAction::class);
         $group->delete('/deleteAffair', \App\Action\AffairDeleteAction::class);
         //Lot
@@ -53,8 +54,6 @@ return function (App $app) {
         $group->get('/getLastPvsByUserId', \App\Action\PvsGetLastsByUserIdAction::class);
         $group->get('/getPvReleasedDetails', \App\Action\PvGetReleasedDetails::class);
         $group->post('/validatePv', \App\Action\PvValidateAction::class);
-
-        // $group->options('/getLastPvsByUserId', \App\Action\PreflightAction::class);
         //Pv Has User
         $group->post('/addPvHasUser', \App\Action\PvHasUserCreateAction::class);
         $group->delete('/deleteParticipant', \App\Action\ParticipantDeleteAction::class);
@@ -68,8 +67,7 @@ return function (App $app) {
         $group->post('/addToken', \App\Action\TokenCreateAction2::class);
         $group->delete('/deleteToken', \App\Action\TokenDeleteAction::class);
         $group->get('/getTokensByUserId', \App\Action\TokensGetByUserIdAction::class);
-        // })->add(JwtMiddleware::class);
-    });
+    })->add(JwtMiddleware::class);
 };
     //Pv Has Item
     //TODO: créer une requete pour pouvoir mettre un item dans plusieurs Pv
