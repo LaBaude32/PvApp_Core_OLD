ebd6eefe63988ba798bd6ea7e17f6df7c2bcb41c
Baudouin Coupey <baudouin.coupey@ik.me>
2022-08-08 15:48:48 +0200

    update packages

--- config/container.php
+++ config/container.php
@@ -3,13 +3,14 @@
 use Slim\App;
 use App\Auth\JwtAuth;
 use Slim\Factory\AppFactory;
-use Selective\Config\Configuration;
+use Slim\Middleware\ErrorMiddleware;
 use Psr\Container\ContainerInterface;
+use Selective\BasePath\BasePathMiddleware;
 use Psr\Http\Message\ResponseFactoryInterface;
 
 return [
-    Configuration::class => function () {
-        return new Configuration(require __DIR__ . '/settings.php');
+    'settings' => function () {
+        return require __DIR__ . '/settings.php';
     },
 
     App::class => function (ContainerInterface $container) {
@@ -18,38 +19,55 @@ return [
 
         // Optional: Set the base path to run the app in a sub-directory
         // The public directory must not be part of the base path
-        $app->setBasePath('/pvappcore');
+        // $app->setBasePath('/pvappcore');
 
         return $app;
     },
 
-    ResponseFactoryInterface::class => function (ContainerInterface $container) {
-        return $container->get(App::class)->getResponseFactory();
+    // ResponseFactoryInterface::class => function (ContainerInterface $container) {
+    //     return $container->get(App::class)->getResponseFactory();
+    // },
+
+    ErrorMiddleware::class => function (ContainerInterface $container) {
+        $app = $container->get(App::class);
+        $settings = $container->get('settings')['error'];
+
+        return new ErrorMiddleware(
+            $app->getCallableResolver(),
+            $app->getResponseFactory(),
+            (bool)$settings['display_error_details'],
+            (bool)$settings['log_errors'],
+            (bool)$settings['log_error_details']
+        );
     },
 
     // And add this entry
     JwtAuth::class => function (ContainerInterface $container) {
-        $config = $container->get(Configuration::class);
+        $settings = $container->get(Configuration::class);
 
-        $issuer = $config->getString('jwt.issuer');
-        $lifetime = $config->getInt('jwt.lifetime');
-        $privateKey = $config->getString('jwt.private_key');
-        $publicKey = $config->getString('jwt.public_key');
+        $issuer = $settings->getString('jwt.issuer');
+        $lifetime = $settings->getInt('jwt.lifetime');
+        $privateKey = $settings->getString('jwt.private_key');
+        $publicKey = $settings->getString('jwt.public_key');
 
         return new JwtAuth($issuer, $lifetime, $privateKey, $publicKey);
     },
 
     PDO::class => function (ContainerInterface $container) {
-        $config = $container->get(Configuration::class);
+        $settings = $container->get(Configuration::class);
 
-        $host = $config->getString('db.host');
-        $dbname =  $config->getString('db.database');
-        $username = $config->getString('db.username');
-        $password = $config->getString('db.password');
-        $charset = $config->getString('db.charset');
-        $flags = $config->getArray('db.flags');
+        $host = $settings->getString('db.host');
+        $dbname =  $settings->getString('db.database');
+        $username = $settings->getString('db.username');
+        $password = $settings->getString('db.password');
+        $charset = $settings->getString('db.charset');
+        $flags = $settings->getArray('db.flags');
         $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
 
         return new PDO($dsn, $username, $password, $flags);
-    }
+    },
+
+    BasePathMiddleware::class => function (ContainerInterface $container) {
+        return new BasePathMiddleware($container->get(App::class));
+    },
 ];
--- config/middleware.php
+++ config/middleware.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Middleware\LoginMiddleware;
-use Selective\Config\Configuration;
 use Slim\App;
+use App\Middleware\LoginMiddleware;
+// use Selective\Config\Configuration;
+// use Slim\Middleware\ErrorMiddleware;
+use Selective\BasePath\BasePathMiddleware;
 
 return function (App $app) {
     // Parse json, form data and xml
@@ -14,15 +16,20 @@ return function (App $app) {
     // Add routing middleware
     $app->addRoutingMiddleware();
 
+    $app->add(BasePathMiddleware::class);
+
     $container = $app->getContainer();
 
     // Add error handler middleware
-    $settings = $container->get(Configuration::class)->getArray('error_handler_middleware');
-    $displayErrorDetails = (bool) $settings['display_error_details'];
-    $logErrors = (bool) $settings['log_errors'];
-    $logErrorDetails = (bool) $settings['log_error_details'];
+    // $settings = $container->get(Configuration::class)->getArray('error_handler_middleware');
+    // $displayErrorDetails = (bool) $settings['display_error_details'];
+    // $logErrors = (bool) $settings['log_errors'];
+    // $logErrorDetails = (bool) $settings['log_error_details'];
 
-    $app->addErrorMiddleware($displayErrorDetails, $logErrors, $logErrorDetails);
+    // $app->addErrorMiddleware($displayErrorDetails, $logErrors, $logErrorDetails);
+
+    // Handle exceptions
+    $app->add(ErrorMiddleware::class);
 
     $app->add(new LoginMiddleware());
 };
